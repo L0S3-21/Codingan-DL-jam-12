73d18c9d072db64a15598f95298493d2eb848fd6
L0S3-21 <julius.samuel1234567@gmail.com>
2025-10-08 11:57:49 +0700

    Add files via upload

--- Nomor3.java
+++ Nomor3.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+import java.util.Scanner; // untuk memanggil objek scanner
 
-public class Nomor3 {
-    public static void main(String[] args) {
+public class Nomor3 { // untuk membuat class
+    public static void main(String[] args) { // untuk memulai program
         try (Scanner Baju = new Scanner(System.in)) {
-            int harga = 0;
+            // scanner untuk memasukkan input user
+            // try untuk mencegah error pada program atau objek yang berada pada blok 
+            int harga = 0; // untuk mendeklarasi sebuah variabel
             System.out.print("masukkan ukuran baju (S, M, L, XL, XXL)= ");
-            String ukuran = Baju.nextLine();
-            System.out.print("masukkan jumlah baju = ");
-            int jumlah = Baju.nextInt();
+            String ukuran = Baju.nextLine(); 
+            System.out.print("masukkan jumlah baju = "); // untuk mencetak tulisan
+            int jumlah = Baju.nextInt(); // untuk mendeklarasi variabel ke sebuah tipe dan memasukkan input user kedalam variabel
 
-            switch (ukuran.toUpperCase()) {
-                case "S":
-                    harga = jumlah * 30000;
-                    break;
+            switch (ukuran.toUpperCase()) { // untuk memeriksa nilai sebuah variabel yang dipilih dari kondisi yang terpenuhi
+                case "S": // untuk membuat kondisi yang harus dipenuhi
+                    harga = jumlah * 30000; // untuk membuat nilai dari variabel jika kondisi terpenuhi
+                    break; // untuk menghentikan program jika kondisi sudah terpenuhi
                 case "M":
                     harga = jumlah * 38000;
                     break;
